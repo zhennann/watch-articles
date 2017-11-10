265125024ce22759d6f1c47c78db7c17a8b494b7
zhennann <zhen.nann@icloud.com>
2017-11-10 21:22:14 +0800

    adjust

--- src/patterns.js
+++ src/patterns.js
@@ -1,5 +1,5 @@
 module.exports = {
+  'github-repo': require('./pattern/github-repo'),
   'cnode-topic': require('./pattern/cnode-topic'),
   'juejin-im-post': require('./pattern/juejin-im-post'),
-  'github-repo': require('./pattern/github-repo'),
 };
